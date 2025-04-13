6468d1e590a98943a17f9a4e57c1a11da78bf4ca
bima-taruna <bimataruna21@gmail.com>
2025-04-13 18:01:51 +0700

    Replace placeholder text with BasicForm component in DataInputs

--- src/components/DataInputs/DataInputs.jsx
+++ src/components/DataInputs/DataInputs.jsx
@@ -1,11 +1,9 @@
 import "../../styles/dataInputs.css";
+import BasicForm from "./BasicForm";
 function DataInputs() {
   return (
     <section className="data-inputs">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi natus
-      omnis molestiae eaque consequuntur, dolorum odio porro tenetur aliquid
-      adipisci? Commodi dolore totam doloremque doloribus ad voluptatum minima
-      quae ex.
+      <BasicForm />
     </section>
   );
 }
