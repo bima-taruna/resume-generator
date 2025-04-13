c0cb043f24492e0697206af27dba2755af13190f
bima-taruna <bimataruna21@gmail.com>
2025-04-13 18:01:35 +0700

    Add TextInput component for user input in resume generator

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,16 @@
+function TextInput({ label, name, value, onChange }) {
+  return (
+    <div className="input-container">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default TextInput;
