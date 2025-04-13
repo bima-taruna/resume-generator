866a3c7376bea5e0425b7f0472c2b13f5c59e521
bima-taruna <bimataruna21@gmail.com>
2025-04-13 19:54:33 +0700

    Refactor App component to manage basic data state and handle input changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { PDFViewer } from "@react-pdf/renderer";
 import Resume from "./components/Resume/Resume";
 import DataInputs from "./components/DataInputs/DataInputs";
+import { useState } from "react";
+import data from "./data/dummy-data";
 function App() {
+  const [basicData, setBasicData] = useState(data.basic);
+  const handleBasicChange = (e) => {
+    const { name, value } = e.target;
+
+    setBasicData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
-      <DataInputs />
-      <Resume />
+      <DataInputs basicData={basicData} onChange={handleBasicChange} />
+      <Resume basicData={basicData} />
     </>
   );
 }
