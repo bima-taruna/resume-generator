2388e7a04cb72194c88b99c0e9129cd351761f4c
bima-taruna <bimataruna21@gmail.com>
2025-04-13 19:54:55 +0700

    Refactor BasicForm component to accept props for better data handling

--- src/components/DataInputs/BasicForm.jsx
+++ src/components/DataInputs/BasicForm.jsx
@@ -1,14 +1,44 @@
 import TextInput from "../TextInput";
 
-function BasicForm() {
+function BasicForm({ basicData, onChange }) {
   return (
     <form className="basic-form">
-      <TextInput label={"Full Name"} name={"full-name"} />
-      <TextInput label={"Headline"} name={"headline"} />
-      <TextInput label={"Email"} name={"email"} />
-      <TextInput label={"Website"} name={"website"} />
-      <TextInput label={"Phone"} name={"phone"} />
-      <TextInput label={"Location"} name={"location"} />
+      <TextInput
+        label={"Full Name"}
+        name={"fullName"}
+        value={basicData.fullName}
+        onChange={onChange}
+      />
+      <TextInput
+        label={"Headline"}
+        name={"headline"}
+        value={basicData.headline}
+        onChange={onChange}
+      />
+      <TextInput
+        label={"Email"}
+        name={"email"}
+        value={basicData.email}
+        onChange={onChange}
+      />
+      <TextInput
+        label={"Website"}
+        name={"website"}
+        value={basicData.website}
+        onChange={onChange}
+      />
+      <TextInput
+        label={"Phone"}
+        name={"phone"}
+        value={basicData.phoneNumber}
+        onChange={onChange}
+      />
+      <TextInput
+        label={"Location"}
+        name={"location"}
+        value={basicData.location}
+        onChange={onChange}
+      />
     </form>
   );
 }
