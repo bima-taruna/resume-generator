d784be791a358d06b990db6c8add86810cd72132
bima-taruna <bimataruna21@gmail.com>
2025-04-13 19:54:11 +0700

    Add dummy data for resume generator

---
+++ src/data/dummy-data.js
@@ -0,0 +1,32 @@
+const data = {
+  basic: {
+    fullName: "John Doe",
+    headline: "Fullstack Web Developer",
+    location: "Medan, Indonesia",
+    phoneNumber: "085762068329",
+    email: "johnDoe@email.com",
+    website: null,
+  },
+  summary:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident veritatis numquam iure ad magni tenetur explicabo, voluptates illum tempora possimus atque, illo eius neque, nesciunt mollitia qui quia nemoeos.",
+  experience: [
+    {
+      company: "PT mencari cinta sejati",
+      date: "August 2017 - January 2024",
+      position: "Fullstack Dev",
+      location: "Medan",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident veritatis numquam iure ad magni tenetur explicabo, voluptates illum tempora possimus atque, illo eius neque, nesciunt mollitia qui quia nemoeos.",
+    },
+    {
+      company: "PT mencari cinta sejati",
+      date: "August 2017 - January 2024",
+      position: "Fullstack Dev",
+      location: "Medan",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident veritatis numquam iure ad magni tenetur explicabo, voluptates illum tempora possimus atque, illo eius neque, nesciunt mollitia qui quia nemoeos.",
+    },
+  ],
+};
+
+export default data;
