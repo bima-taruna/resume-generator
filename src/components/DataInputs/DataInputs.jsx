47f280fef4388fd5d233232ab38c33fb3038de5b
bima-taruna <bimataruna21@gmail.com>
2025-04-15 20:06:50 +0700

    Add Summary component to DataInputs for displaying summary data

--- src/components/DataInputs/DataInputs.jsx
+++ src/components/DataInputs/DataInputs.jsx
@@ -1,9 +1,11 @@
 import "../../styles/dataInputs.css";
 import BasicForm from "./BasicForm";
-function DataInputs({ basicData, onChange }) {
+import Summary from "./Summary";
+function DataInputs({ basicData, summaryData, onChange }) {
   return (
     <section className="data-inputs">
       <BasicForm basicData={basicData} onChange={onChange} />
+      <Summary summaryData={summaryData} onChange={onChange} />
     </section>
   );
 }
