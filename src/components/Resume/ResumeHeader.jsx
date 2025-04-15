b65c5d252749cabda09b85bf075877172d155cf5
bima-taruna <bimataruna21@gmail.com>
2025-04-15 13:09:43 +0700

    Refactor ResumeHeader component to wrap basic data in a container and add user picture placeholder

--- src/components/Resume/ResumeHeader.jsx
+++ src/components/Resume/ResumeHeader.jsx
@@ -1,21 +1,24 @@
 import "../../styles/resumeHeader.css";
 function ResumeHeader({ basicData }) {
   return (
-    <header className="resume-header">
-      <h1 className="full-name">{basicData.fullName}</h1>
-      <h2 className="headline">{basicData.headline}</h2>
-      <ul>
-        {basicData.location && (
-          <li className="location">{basicData.location}</li>
-        )}
-        {basicData.phoneNumber && (
-          <li className="phone-number">| {basicData.phoneNumber}</li>
-        )}
-        {basicData.email && <li className="email">| {basicData.email}</li>}
-        {basicData.website && (
-          <li className="website">| {basicData.website}</li>
-        )}
-      </ul>
+    <header id="resume-header">
+      <div className="user-pic"></div>
+      <div className="basic-data">
+        <h1 className="full-name">{basicData.fullName}</h1>
+        <h2 className="headline">{basicData.headline}</h2>
+        <ul>
+          {basicData.location && (
+            <li className="location">{basicData.location}</li>
+          )}
+          {basicData.phoneNumber && (
+            <li className="phone-number">| {basicData.phoneNumber}</li>
+          )}
+          {basicData.email && <li className="email">| {basicData.email}</li>}
+          {basicData.website && (
+            <li className="website">| {basicData.website}</li>
+          )}
+        </ul>
+      </div>
     </header>
   );
 }
