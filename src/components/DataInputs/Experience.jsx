ec977110729b0f9661321a4bab855259d841eff5
bima-taruna <bimataruna21@gmail.com>
2025-04-16 17:10:06 +0700

    Add Button component to Experience for adding new items

--- src/components/DataInputs/Experience.jsx
+++ src/components/DataInputs/Experience.jsx
@@ -1,6 +1,8 @@
 import { IoBriefcaseOutline } from "react-icons/io5";
 import ItemCard from "../ItemCard";
-
+import Button from "../Button";
+import { IoMdAdd } from "react-icons/io";
+import "../../styles/experience.css";
 function Experience({ experienceData }) {
   return (
     <section id="experience">
@@ -13,6 +15,11 @@ function Experience({ experienceData }) {
           <ItemCard title={item.company} subtitle={item.position} />
         ))}
       </ul>
+      <Button
+        className={"btn-black"}
+        icon={<IoMdAdd />}
+        text={"Add a new item"}
+      />
     </section>
   );
 }
