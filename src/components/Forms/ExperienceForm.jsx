d127edca1b8092ac46bb97d02740b930797730f2
bima-taruna <bimataruna21@gmail.com>
2025-04-18 15:28:30 +0700

    Refactor ExperienceForm layout to group related inputs and improve readability

--- src/components/Forms/ExperienceForm.jsx
+++ src/components/Forms/ExperienceForm.jsx
@@ -1,15 +1,25 @@
 import TextArea from "../TextArea";
 import TextInput from "../TextInput";
-
+import "../../styles/experienceForm.css";
+import Button from "../Button";
 function ExperienceForm() {
   return (
     <form className="experience-form">
-      <TextInput label={"Company"} name={"exp-company"} />
-      <TextInput label={"Date or Date Range"} name={"exp-date"} />
-      <TextInput label={"Position"} name={"exp-position"} />
-      <TextInput label={"Location"} name={"exp-location"} />
+      <div>
+        <TextInput label={"Company"} name={"exp-company"} />
+        <TextInput label={"Position"} name={"exp-position"} />
+      </div>
+      <div>
+        <TextInput
+          label={"Date or Date Range"}
+          name={"exp-date"}
+          placeholder={"March 2023 - Present"}
+        />
+        <TextInput label={"Location"} name={"exp-location"} />
+      </div>
       <TextInput label={"Website"} />
       <TextArea label={"Summary"} name={"exp-summary"} />
+      <Button className={"btn-white"} text={"create"} />
     </form>
   );
 }
