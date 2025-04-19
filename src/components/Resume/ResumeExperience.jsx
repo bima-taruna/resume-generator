dbb7e3d94194d3baf267a0b65fcaaafb5ecae66a
bima-taruna <bimataruna21@gmail.com>
2025-04-19 13:32:31 +0700

    Add ResumeExperience component to display user experience data

---
+++ src/components/Resume/ResumeExperience.jsx
@@ -0,0 +1,27 @@
+function ResumeExperience({ experienceData }) {
+  return (
+    <section id="experience">
+      <h3>Experience</h3>
+      <hr />
+      <ul className="exp-list">
+        {experienceData &&
+          experienceData.map((experience) => (
+            <li>
+              <div className="exp-header">
+                <h3 className="exp-company">{experience.company}</h3>
+                <h3 className="exp-date">{experience.date}</h3>
+              </div>
+              <div className="exp-detail">
+                <h4 className="exp-position">{experience.position}</h4>
+                <h4 className="exp-location">{experience.location}</h4>
+              </div>
+              <div className="exp-website">{experience.website}</div>
+              <div className="exp-desc">{experience.summary}</div>
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+}
+
+export default ResumeExperience;
