0356c8b09a30df05f7b09cf9ce3ea768cc4bd9e3
bima-taruna <bimataruna21@gmail.com>
2025-04-19 11:53:51 +0700

    Add type prop to Button component for improved flexibility

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import "../styles/button.css";
-function Button({ className, icon, text, onClick }) {
+function Button({ className, icon, text, onClick, type = "button" }) {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} onClick={onClick} type={type}>
       {icon && icon}
       {text}
     </button>
