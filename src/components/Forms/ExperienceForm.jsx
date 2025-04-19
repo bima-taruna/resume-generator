55534bea809d745c932592f1734ade08c9a8f016
bima-taruna <bimataruna21@gmail.com>
2025-04-19 11:53:59 +0700

    Refactor ExperienceForm component to improve input naming consistency and form submission handling

--- src/components/Forms/ExperienceForm.jsx
+++ src/components/Forms/ExperienceForm.jsx
@@ -2,24 +2,24 @@ import TextArea from "../TextArea";
 import TextInput from "../TextInput";
 import "../../styles/experienceForm.css";
 import Button from "../Button";
-function ExperienceForm() {
+function ExperienceForm({ handleSubmit }) {
   return (
-    <form className="experience-form">
+    <form id="experience-form" onSubmit={handleSubmit}>
       <div>
-        <TextInput label={"Company"} name={"exp-company"} />
-        <TextInput label={"Position"} name={"exp-position"} />
+        <TextInput label={"Company"} name={"company"} />
+        <TextInput label={"Position"} name={"position"} />
       </div>
       <div>
         <TextInput
           label={"Date or Date Range"}
-          name={"exp-date"}
+          name={"date"}
           placeholder={"March 2023 - Present"}
         />
-        <TextInput label={"Location"} name={"exp-location"} />
+        <TextInput label={"Location"} name={"location"} />
       </div>
-      <TextInput label={"Website"} />
-      <TextArea label={"Summary"} name={"exp-summary"} />
-      <Button className={"btn-white"} text={"create"} />
+      <TextInput label={"Website"} name={"website"} />
+      <TextArea label={"Summary"} name={"summary"} />
+      <Button className={"btn-white"} text={"Create"} type="submit" />
     </form>
   );
 }
