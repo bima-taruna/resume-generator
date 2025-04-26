0fd34c879d93765e7dac6b8cfb3971652a86dec0
bima-taruna <bimataruna21@gmail.com>
2025-04-26 12:17:14 +0700

    Refactor ExperienceForm to accept data prop for editing existing experience items

--- src/components/Forms/ExperienceForm.jsx
+++ src/components/Forms/ExperienceForm.jsx
@@ -2,24 +2,29 @@ import TextArea from "../TextArea";
 import TextInput from "../TextInput";
 import "../../styles/experienceForm.css";
 import Button from "../Button";
-function ExperienceForm({ handleSubmit }) {
+function ExperienceForm({ handleSubmit, data = {} }) {
   return (
     <form id="experience-form" onSubmit={handleSubmit}>
       <div>
-        <TextInput label={"Company"} name={"company"} />
-        <TextInput label={"Position"} name={"position"} />
+        <TextInput label={"Company"} name={"company"} value={data.company} />
+        <TextInput label={"Position"} name={"position"} value={data.position} />
       </div>
       <div>
         <TextInput
           label={"Date or Date Range"}
           name={"date"}
           placeholder={"March 2023 - Present"}
+          value={data.date}
         />
-        <TextInput label={"Location"} name={"location"} />
+        <TextInput label={"Location"} name={"location"} value={data.location} />
       </div>
-      <TextInput label={"Website"} name={"website"} />
-      <TextArea label={"Summary"} name={"summary"} />
-      <Button className={"btn-white"} text={"Create"} type="submit" />
+      <TextInput label={"Website"} name={"website"} value={data.website} />
+      <TextArea label={"Summary"} name={"summary"} value={data.summary} />
+      <Button
+        className={"btn-white"}
+        text={data ? "Update" : "Create"}
+        type="submit"
+      />
     </form>
   );
 }
