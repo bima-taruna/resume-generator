2d8042f6a4c882d163a4ea5edd30a904f6f2767a
bima-taruna <bimataruna21@gmail.com>
2025-04-27 21:45:08 +0700

    Refactor Profiles component to accept profilesData prop for dynamic rendering

--- src/components/DataInputs/Profiles.jsx
+++ src/components/DataInputs/Profiles.jsx
@@ -1,12 +1,18 @@
 import { IoShareSocialOutline } from "react-icons/io5";
+import ItemCard from "../ItemCard";
 
-function Profiles() {
+function Profiles({ profilesData }) {
   return (
     <section id="profiles">
       <header>
         <IoShareSocialOutline />
         <h2>Profiles</h2>
       </header>
+      <ul>
+        {profilesData?.map((item, index) => (
+          <ItemCard key={index} title={item.type} subtitle={item.name} />
+        ))}
+      </ul>
     </section>
   );
 }
