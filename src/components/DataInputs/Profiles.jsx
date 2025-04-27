99ae584f32347825d5bc30d1b89da33b40358b23
bima-taruna <bimataruna21@gmail.com>
2025-04-27 21:18:09 +0700

    Add Profiles component to display user profiles section

---
+++ src/components/DataInputs/Profiles.jsx
@@ -0,0 +1,14 @@
+import { IoShareSocialOutline } from "react-icons/io5";
+
+function Profiles() {
+  return (
+    <section id="profiles">
+      <header>
+        <IoShareSocialOutline />
+        <h2>Profiles</h2>
+      </header>
+    </section>
+  );
+}
+
+export default Profiles;
