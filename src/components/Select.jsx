9ee3365de48d6a39795c0b964523e5c5026b249f
bima-taruna <bimataruna21@gmail.com>
2025-04-28 12:22:46 +0700

    Add Select component for rendering dropdown menus with dynamic options

---
+++ src/components/Select.jsx
@@ -0,0 +1,17 @@
+function Select({ label, name, options, defaultValue }) {
+  return (
+    <div className="select-container">
+      <label htmlFor={name}>{label}</label>
+      <select name={name} id={name} defaultValue={defaultValue}>
+        <option value="">--Please choose--</option>
+        {options.map((item, index) => (
+          <option key={index} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
