b95019c831f97811f26cacf778a8166301125a03
bima-taruna <bimataruna21@gmail.com>
2025-04-28 12:22:02 +0700

    Add ProfileForm component for managing user profiles with dynamic input fields

---
+++ src/components/Forms/ProfileForm.jsx
@@ -0,0 +1,45 @@
+import Button from "../Button";
+import Select from "../Select";
+import TextInput from "../TextInput";
+
+function ProfileForm({ handleSubmit, data = {}, handleDelete, itemIndex }) {
+  return (
+    <form id="profile-form" onSubmit={handleSubmit}>
+      <div>
+        <Select
+          label={"Network"}
+          name={"network"}
+          options={["github", "linkedin"]}
+          defaultValue={data.network || ""}
+        />
+        <TextInput
+          label={"Username"}
+          name={"username"}
+          defaultValue={data.username || ""}
+        />
+      </div>
+      <TextInput
+        label={"Website"}
+        name={"website"}
+        placeholder={"https://linkedin.com/john-doe"}
+        defaultValue={data.website || ""}
+      />
+      <div className="profile-form-buttons">
+        {Object.keys(data).length > 0 && (
+          <Button
+            className={"btn-red"}
+            text={"Delete"}
+            onClick={() => handleDelete(itemIndex)}
+          />
+        )}
+        <Button
+          className={"btn-white"}
+          text={Object.keys(data).length > 0 ? "Update" : "Create"}
+          type="submit"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default ProfileForm;
