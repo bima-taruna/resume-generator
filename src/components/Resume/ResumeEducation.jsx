f4cef7eb853d77f1f1617d38079450d42f0403fa
bima-taruna <bimataruna21@gmail.com>
2025-05-02 08:53:08 +0700

    Add ResumeEducation component to display education section

---
+++ src/components/Resume/ResumeEducation.jsx
@@ -0,0 +1,26 @@
+function ResumeEducation({ educationData }) {
+  return (
+    <section id="education">
+      <h3>Education</h3>
+      <hr />
+      <ul className="edu-list">
+        {educationData &&
+          educationData.map((item, index) => (
+            <li key={index}>
+              <div className="edu-header">
+                <h3 className="edu-institution">{item.institution}</h3>
+                <h3 className="edu-date">{item.date}</h3>
+              </div>
+              <div className="edu-detail">
+                <h4 className="edu-study">{item.study}</h4>
+                <h4 className="edu-type">{item.type}</h4>
+              </div>
+              <h4 className="edu-score">{item.score}</h4>
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
+}
+
+export default ResumeEducation;
