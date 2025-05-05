745126f2a97b4ee7c4cc6961326d2ed1383320a3
bima-taruna <bimataruna21@gmail.com>
2025-05-05 22:14:47 +0700

    Refactor Resume component to integrate ResumeProject and remove hardcoded project section

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,15 +1,18 @@
+import { IoIosLink } from "react-icons/io";
 import "../../styles/resume.css";
 import ResumeEducation from "./ResumeEducation";
 import ResumeExperience from "./ResumeExperience";
 import ResumeHeader from "./ResumeHeader";
 import ResumeProfiles from "./ResumeProfiles";
 import ResumeSummary from "./ResumeSummary";
+import ResumeProject from "./ResumeProject";
 function Resume({
   basicData,
   summary,
   experienceData,
   profilesData,
   educationData,
+  projectsData,
 }) {
   return (
     <section className="resume-canvas">
@@ -18,27 +21,7 @@ function Resume({
       <ResumeSummary summary={summary} />
       <ResumeExperience experienceData={experienceData} />
       <ResumeEducation educationData={educationData} />
-      <section id="projects">
-        <h3>Projects </h3>
-        <hr />
-        <ul className="project-list">
-          <li>
-            <div className="project-header">
-              <h3 className="project-name">My Project</h3>
-              <h3 className="project-date">August 2017 - January 2024</h3>
-            </div>
-            <div className="project-link">
-              <a href="http://">myyyy</a>
-            </div>
-            <div className="project-desc">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa
-              tempora, itaque, inventore sint, architecto excepturi aliquam eius
-              assumenda quos harum cumque autem. Veniam, laboriosam quisquam! Id
-              a fugiat repudiandae placeat?
-            </div>
-          </li>
-        </ul>
-      </section>
+      <ResumeProject projectsData={projectsData} />
       <section id="skills">
         <h3>Skills</h3>
         <hr />
