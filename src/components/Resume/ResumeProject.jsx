dc84ddf55ed20ea63ebf7c383a3fc2e6e6567c80
bima-taruna <bimataruna21@gmail.com>
2025-05-05 22:14:30 +0700

    Add ResumeProject component to display project data

---
+++ src/components/Resume/ResumeProject.jsx
@@ -0,0 +1,31 @@
+import { IoIosLink } from "react-icons/io";
+
+function ResumeProject({ projectsData }) {
+  if (!projectsData || projectsData.length < 1) {
+    return null;
+  }
+  return (
+    <section id="projects">
+      <h3>Projects</h3>
+      <hr />
+      <ul className="project-list">
+        {projectsData?.map((item, index) => (
+          <li key={index}>
+            <div className="project-header">
+              <h3 className="project-name">{item.name}</h3>
+              <h3 className="project-date">{item.date}</h3>
+            </div>
+            <div className="project-desc">{item.description}</div>
+            <div className="project-link">
+              <IoIosLink />
+              <a href={item.website}>{item.website}</a>
+            </div>
+            <div className="project-summary">{item.summary}</div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default ResumeProject;
