251ef6e5ecdf527d714ab8c5ab4926e6ec3d9815
bima-taruna <bimataruna21@gmail.com>
2025-05-11 21:16:49 +0700

    Add Skills component for managing and displaying user skills

---
+++ src/components/DataInputs/Skills.jsx
@@ -0,0 +1,36 @@
+import { GiSkills } from "react-icons/gi";
+import ItemCard from "../ItemCard";
+import modalTypes from "../../helper/modalTypes";
+import Button from "../Button";
+import { IoMdAdd } from "react-icons/io";
+
+function Skills({ skillData, openModal, openUpdateModal, setIndexItem }) {
+  return (
+    <section id="skill">
+      <header>
+        <GiSkills />
+        <h2>Skills</h2>
+      </header>
+      <ul>
+        {skillData?.map((item, index) => (
+          <ItemCard
+            key={index}
+            title={item}
+            handleClick={() => {
+              openUpdateModal(modalTypes.SKILL);
+              setIndexItem(index);
+            }}
+          />
+        ))}
+      </ul>
+      <Button
+        className={"btn-black"}
+        icon={<IoMdAdd />}
+        text={"Add a new item"}
+        onClick={() => openModal(modalTypes.PROJECT)}
+      />
+    </section>
+  );
+}
+
+export default Skills;
