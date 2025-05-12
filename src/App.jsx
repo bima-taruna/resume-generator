1618ac5461cdfcc48d737e2eae1a123de3101b56
bima-taruna <bimataruna21@gmail.com>
2025-05-12 21:56:49 +0700

    Refactor App component to utilize useResumeData hook for state management and remove redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -1,188 +1,22 @@
 import { PDFViewer } from "@react-pdf/renderer";
 import Resume from "./components/Resume/Resume";
 import DataInputs from "./components/DataInputs/DataInputs";
-import { useState } from "react";
-import data from "./data/dummy-data";
-import Modal from "./components/Modal";
-import ExperienceForm from "./components/Forms/ExperienceForm";
-import ProfileForm from "./components/Forms/ProfileForm";
-import EducationForm from "./components/Forms/EducationForm";
-import ProjectForm from "./components/Forms/ProjectForm";
 import CustomModal from "./components/CustomModal";
+import useResumeData from "./hooks/useResumeData";
 function App() {
-  const [resumeData, setResumeData] = useState({
-    basic: data.basic,
-    summary: data.summary,
-    experience: data.experience,
-    profiles: data.profiles,
-    education: data.education,
-    projects: data.projects,
-    skills: data.skills,
-  });
-
-  const [activeModal, setActiveModal] = useState(null);
-  const [activeUpdateModal, setActiveUpdateModal] = useState(null);
-  const [indexItem, setIndexItem] = useState(null);
-
-  const handleChange = (e) => {
-    const { name, value, id } = e.target;
-
-    setResumeData((prev) => {
-      switch (id) {
-        case "summary":
-          return {
-            ...prev,
-            summary: value,
-          };
-
-        default:
-          return {
-            ...prev,
-            basic: {
-              ...prev.basic,
-              [name]: value,
-            },
-          };
-      }
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = e.target.id;
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    switch (id) {
-      case "experience-form":
-        setResumeData((prev) => {
-          return {
-            ...prev,
-            experience: [...prev.experience, data],
-          };
-        });
-        break;
-      case "education-form":
-        setResumeData((prev) => {
-          return {
-            ...prev,
-            education: [...prev.education, data],
-          };
-        });
-        break;
-      case "project-form":
-        setResumeData((prev) => {
-          return {
-            ...prev,
-            projects: [...prev.projects, data],
-          };
-        });
-        break;
-      default:
-        setResumeData((prev) => {
-          return {
-            ...prev,
-            profiles: [...prev.profiles, data],
-          };
-        });
-        break;
-    }
-    setActiveModal(null);
-  };
-
-  const handleUpdateSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    const id = e.target.id;
-    switch (id) {
-      case "education-form":
-        setResumeData((prev) => {
-          const updatedEducation = [...prev.education];
-          updatedEducation[indexItem] = data;
-          return {
-            ...prev,
-            education: updatedEducation,
-          };
-        });
-        break;
-      case "profile-form":
-        setResumeData((prev) => {
-          const updatedProfile = [...prev.profiles];
-          updatedProfile[indexItem] = data;
-          return {
-            ...prev,
-            profiles: updatedProfile,
-          };
-        });
-        break;
-      case "project-form":
-        setResumeData((prev) => {
-          const updatedProject = [...prev.projects];
-          updatedProject[indexItem] = data;
-          return {
-            ...prev,
-            projects: updatedProject,
-          };
-        });
-        break;
-      default:
-        setResumeData((prev) => {
-          const updatedExperience = [...prev.experience];
-          updatedExperience[indexItem] = data;
-          return {
-            ...prev,
-            experience: updatedExperience,
-          };
-        });
-        break;
-    }
-    setActiveUpdateModal(null);
-  };
-
-  const handleDelete = (type, index) => {
-    switch (type) {
-      case "experience":
-        setResumeData((prev) => {
-          const updatedExperience = prev.experience.filter(
-            (_, i) => i !== index
-          );
-          return {
-            ...prev,
-            experience: updatedExperience,
-          };
-        });
-
-        break;
-      case "education":
-        setResumeData((prev) => {
-          const updatedEducation = prev.education.filter((_, i) => i !== index);
-          return {
-            ...prev,
-            education: updatedEducation,
-          };
-        });
-        break;
-      case "project":
-        setResumeData((prev) => {
-          const updatedProject = prev.projects.filter((_, i) => i !== index);
-          return {
-            ...prev,
-            projects: updatedProject,
-          };
-        });
-        break;
-      default:
-        setResumeData((prev) => {
-          const updatedProfile = prev.profiles.filter((_, i) => i !== index);
-          return {
-            ...prev,
-            profiles: updatedProfile,
-          };
-        });
-        break;
-    }
-    setActiveUpdateModal(null);
-  };
+  const {
+    resumeData,
+    activeModal,
+    setActiveModal,
+    activeUpdateModal,
+    setActiveUpdateModal,
+    indexItem,
+    setIndexItem,
+    handleChange,
+    handleSubmit,
+    handleUpdateSubmit,
+    handleDelete,
+  } = useResumeData();
 
   return (
     <>
@@ -210,12 +44,14 @@ function App() {
       />
       <CustomModal
         type={activeModal}
+        modalType={"CREATE"}
         isOpen={activeModal !== null}
         onClose={() => setActiveModal(null)}
         handleSubmit={handleSubmit}
       />
       <CustomModal
         type={activeUpdateModal}
+        modalType={"UPDATE"}
         isOpen={activeUpdateModal !== null}
         onClose={() => setActiveUpdateModal(null)}
         handleSubmit={handleUpdateSubmit}
