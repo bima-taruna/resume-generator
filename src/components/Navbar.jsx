7a7902d486a030004e992b106e196058e7f22b07
bima-taruna <bimataruna21@gmail.com>
2025-05-15 16:27:48 +0700

    Add Navbar component for the resume generator

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "../styles/navbar.css";
+function Navbar() {
+  return (
+    <nav>
+      <h2>RESUME GENERATOR</h2>
+      <div>
+        <button>download</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
