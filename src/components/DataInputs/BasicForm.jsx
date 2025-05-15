3c8407c4abadc4e2ad73a92bdb0ad9b6247f1496
bima-taruna <bimataruna21@gmail.com>
2025-05-15 11:26:36 +0700

    Refactor BasicForm component for improved structure and readability

--- src/components/DataInputs/BasicForm.jsx
+++ src/components/DataInputs/BasicForm.jsx
@@ -4,52 +4,55 @@ import { FaRegUser } from "react-icons/fa";
 
 function BasicForm({ basicData, onChange }) {
   return (
-    <section id="basics">
-      <header>
-        <FaRegUser />
-        <h2>Basics</h2>
-      </header>
-      <TextInput
-        label={"Full Name"}
-        name={"fullName"}
-        value={basicData.fullName}
-        onChange={onChange}
-      />
-      <TextInput
-        label={"Headline"}
-        name={"headline"}
-        value={basicData.headline}
-        onChange={onChange}
-      />
-      <div>
+    <>
+      <section id="basics">
+        <header>
+          <FaRegUser />
+          <h2>Basics</h2>
+        </header>
         <TextInput
-          label={"Email"}
-          name={"email"}
-          value={basicData.email}
+          label={"Full Name"}
+          name={"fullName"}
+          value={basicData.fullName}
           onChange={onChange}
         />
         <TextInput
-          label={"Phone"}
-          name={"phoneNumber"}
-          value={basicData.phoneNumber}
+          label={"Headline"}
+          name={"headline"}
+          value={basicData.headline}
           onChange={onChange}
         />
-      </div>
-      <div>
-        <TextInput
-          label={"Website"}
-          name={"website"}
-          value={basicData.website}
-          onChange={onChange}
-        />
-        <TextInput
-          label={"Location"}
-          name={"location"}
-          value={basicData.location}
-          onChange={onChange}
-        />
-      </div>
-    </section>
+        <div>
+          <TextInput
+            label={"Email"}
+            name={"email"}
+            value={basicData.email}
+            onChange={onChange}
+          />
+          <TextInput
+            label={"Phone"}
+            name={"phoneNumber"}
+            value={basicData.phoneNumber}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <TextInput
+            label={"Website"}
+            name={"website"}
+            value={basicData.website}
+            onChange={onChange}
+          />
+          <TextInput
+            label={"Location"}
+            name={"location"}
+            value={basicData.location}
+            onChange={onChange}
+          />
+        </div>
+      </section>
+      <hr />
+    </>
   );
 }
 
