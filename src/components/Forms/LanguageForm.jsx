dd9f4dd7b348bab24d1709a19b156f25860f592d
bima-taruna <bimataruna21@gmail.com>
2025-05-15 11:44:23 +0700

    Add LanguageForm component for managing language entries

---
+++ src/components/Forms/LanguageForm.jsx
@@ -0,0 +1,32 @@
+import "../../styles/form.css";
+import Button from "../Button";
+import TextInput from "../TextInput";
+
+function LanguageForm({ handleSubmit, data, handleDelete, itemIndex }) {
+  return (
+    <form id="languages-form" onSubmit={handleSubmit}>
+      <TextInput
+        label={"Name"}
+        name={"name"}
+        defaultValue={data || ""}
+        required={true}
+      />
+      <div className="languages-form-buttons">
+        {data && (
+          <Button
+            className={"btn-red"}
+            text={"Delete"}
+            onClick={() => handleDelete("languages", itemIndex)}
+          />
+        )}
+        <Button
+          className={"btn-white"}
+          text={data ? "Update" : "Create"}
+          type="submit"
+        />
+      </div>
+    </form>
+  );
+}
+
+export default LanguageForm;
