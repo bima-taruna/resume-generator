02dbe565eb772460e706f066b84a76a6de2d467f
bima-taruna <bimataruna21@gmail.com>
2025-05-15 11:26:48 +0700

    Refactor Summary component to improve structure and readability

--- src/components/DataInputs/Summary.jsx
+++ src/components/DataInputs/Summary.jsx
@@ -3,13 +3,16 @@ import TextArea from "../TextArea";
 
 function Summary({ summaryData, onChange }) {
   return (
-    <section id="summary">
-      <header>
-        <FaRegNewspaper />
-        <h2>Summary</h2>
-      </header>
-      <TextArea name={"summary"} value={summaryData} onChange={onChange} />
-    </section>
+    <>
+      <section id="summary">
+        <header>
+          <FaRegNewspaper />
+          <h2>Summary</h2>
+        </header>
+        <TextArea name={"summary"} value={summaryData} onChange={onChange} />
+      </section>
+      <hr />
+    </>
   );
 }
 
