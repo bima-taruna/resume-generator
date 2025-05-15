a4705d3641bf3ee750bd501a4d32c52826c73ecb
bima-taruna <bimataruna21@gmail.com>
2025-05-15 11:26:43 +0700

    Refactor Profiles component to improve structure and readability

--- src/components/DataInputs/Profiles.jsx
+++ src/components/DataInputs/Profiles.jsx
@@ -6,31 +6,34 @@ import modalTypes from "../../helper/modalTypes";
 
 function Profiles({ profilesData, openModal, openUpdateModal, setIndexItem }) {
   return (
-    <section id="profiles">
-      <header>
-        <IoShareSocialOutline />
-        <h2>Profiles</h2>
-      </header>
-      <ul>
-        {profilesData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item.network}
-            subtitle={item.username}
-            handleClick={() => {
-              openUpdateModal(modalTypes.PROFILE);
-              setIndexItem(index);
-            }}
-          />
-        ))}
-      </ul>
-      <Button
-        className={"btn-black"}
-        icon={<IoMdAdd />}
-        text={"Add a new item"}
-        onClick={() => openModal(modalTypes.PROFILE)}
-      />
-    </section>
+    <>
+      <section id="profiles">
+        <header>
+          <IoShareSocialOutline />
+          <h2>Profiles</h2>
+        </header>
+        <ul>
+          {profilesData?.map((item, index) => (
+            <ItemCard
+              key={index}
+              title={item.network}
+              subtitle={item.username}
+              handleClick={() => {
+                openUpdateModal(modalTypes.PROFILE);
+                setIndexItem(index);
+              }}
+            />
+          ))}
+        </ul>
+        <Button
+          className={"btn-black"}
+          icon={<IoMdAdd />}
+          text={"Add a new item"}
+          onClick={() => openModal(modalTypes.PROFILE)}
+        />
+      </section>
+      <hr />
+    </>
   );
 }
 
