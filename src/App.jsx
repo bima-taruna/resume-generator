14edfdc908eb62cf6f1732284830857371558d3e
bima-taruna <bimataruna21@gmail.com>
2025-05-16 16:27:56 +0700

    Refactor App component: Remove unused PDFViewer import and update DataInputs CSS width to 25rem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { PDFViewer } from "@react-pdf/renderer";
 import Resume from "./components/Resume/Resume";
 import DataInputs from "./components/DataInputs/DataInputs";
 import CustomModal from "./components/CustomModal";
