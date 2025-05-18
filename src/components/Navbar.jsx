32cb2bc68eed7bb8f097791dcffcc0aa8a1cc469
bima-taruna <bimataruna21@gmail.com>
2025-05-18 14:33:02 +0700

    Style Navbar component with PdfDownload button and associated CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { IoDownload } from "react-icons/io5";
 import "../styles/navbar.css";
 import PdfDownload from "./PDF/PdfDownload";
 function Navbar({ resumeData }) {
   return (
     <nav>
       <h2>RESUME GENERATOR</h2>
-      <div>
+      <div className="pdf-download">
+        <IoDownload />
         <PdfDownload resumeData={resumeData} />
       </div>
     </nav>
