f83b5309d6d5c03e354fdaf751aa708992810a77
bima-taruna <bimataruna21@gmail.com>
2025-06-05 10:12:47 +0700

    Add FloatingButton component with hamburger menu icon

---
+++ src/components/FloatingButton.jsx
@@ -0,0 +1,11 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import "../styles/floatingButton.css";
+function FloatingButton() {
+  return (
+    <button className="floating-button">
+      <RxHamburgerMenu />
+    </button>
+  );
+}
+
+export default FloatingButton;
