a760dd087e3066534799560b1ba32a26094e9086
bima-taruna <bimataruna21@gmail.com>
2025-06-14 11:38:45 +0700

    Add profilePic prop to ResumeHeader component for displaying user profile picture

--- src/components/Resume/ResumeHeader.jsx
+++ src/components/Resume/ResumeHeader.jsx
@@ -1,10 +1,18 @@
 import { GrLink, GrLocation, GrPhone } from "react-icons/gr";
 import "../../styles/resumeHeader.css";
 import { MdAlternateEmail } from "react-icons/md";
-function ResumeHeader({ basicData }) {
+function ResumeHeader({ basicData, profilePic }) {
   return (
     <header id="resume-header">
-      <div className="user-pic"></div>
+      <div className="user-pic">
+        {profilePic && (
+          <img
+            src={profilePic}
+            alt="profile picture"
+            style={{ width: "100%", height: "100%" }}
+          />
+        )}
+      </div>
       <div className="basic-data">
         <h1 className="full-name">{basicData.fullName}</h1>
         <h2 className="headline">{basicData.headline}</h2>
