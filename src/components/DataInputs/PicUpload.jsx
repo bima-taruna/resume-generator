c2e3a3bd2d1591370d557b98b2b2795021b14eab
bima-taruna <bimataruna21@gmail.com>
2025-06-15 11:20:31 +0700

    Wrap file input in a div for improved layout in PicUpload component

--- src/components/DataInputs/PicUpload.jsx
+++ src/components/DataInputs/PicUpload.jsx
@@ -2,12 +2,14 @@ import "../../styles/profile-pic.css";
 function PicUpload({ profilePic, handlePicUpload }) {
   return (
     <div className="profile-pic">
-      <input type="file" accept="image/*" onChange={handlePicUpload} />
       {profilePic && (
         <div className="pic">
           <img src={profilePic} alt="Profile" />
         </div>
       )}
+      <div className="upload-container">
+        <input type="file" accept="image/*" onChange={handlePicUpload} />
+      </div>
     </div>
   );
 }
