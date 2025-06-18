fce332daeab5e650399c0c2d03c71fbed9c78599
bima-taruna <bimataruna21@gmail.com>
2025-06-18 12:46:02 +0700

    Add profilePic prop to Navbar component for improved PdfDownload functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import { IoDownload } from "react-icons/io5";
 import "../styles/navbar.css";
 import PdfDownload from "./PDF/PdfDownload";
-function Navbar({ resumeData }) {
+function Navbar({ resumeData, profilePic }) {
   return (
     <nav>
       <h2>RESUME GENERATOR</h2>
       <div className="pdf-download">
         <IoDownload />
-        <PdfDownload resumeData={resumeData} />
+        <PdfDownload resumeData={resumeData} profilePic={profilePic} />
       </div>
     </nav>
   );
