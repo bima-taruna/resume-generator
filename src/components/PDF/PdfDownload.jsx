188b6bb8ee9499d58a7e5612e7141404d49755f1
bima-taruna <bimataruna21@gmail.com>
2025-06-18 12:45:57 +0700

    Add profilePic prop to PdfDownload component for enhanced ResumePDF rendering

--- src/components/PDF/PdfDownload.jsx
+++ src/components/PDF/PdfDownload.jsx
@@ -1,11 +1,11 @@
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import ResumePDF from "./ResumePDF";
 
-const PdfDownload = ({ resumeData }) => (
+const PdfDownload = ({ resumeData, profilePic }) => (
   <div>
     <PDFDownloadLink
       style={{ color: "black", textDecoration: "none" }}
-      document={<ResumePDF {...resumeData} />}
+      document={<ResumePDF {...resumeData} profilePic={profilePic} />}
       fileName="resume.pdf"
     >
       {({ loading }) => (loading ? "Loading document..." : "Download PDF")}
