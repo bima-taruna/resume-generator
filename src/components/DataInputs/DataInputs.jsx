7028a1e2d71206333cb9547ec9704b7f3b3c0175
bima-taruna <bimataruna21@gmail.com>
2025-07-07 15:21:52 +0700

    fix: implement mobile animation for DataInputs component using framer-motion

--- src/components/DataInputs/DataInputs.jsx
+++ src/components/DataInputs/DataInputs.jsx
@@ -12,7 +12,7 @@ import modalTypes from "../../helper/modalTypes";
 import ItemCard from "../ItemCard";
 import { PiCertificateLight, PiGraduationCap } from "react-icons/pi";
 import { GiSkills } from "react-icons/gi";
-import PicUpload from "./PicUpload";
+import { motion as Motion, AnimatePresence } from "framer-motion";
 function DataInputs({
   basicData,
   summaryData,
@@ -31,136 +31,152 @@ function DataInputs({
   profilePic,
   handlePicUpload,
   deletePic,
+  isMobile,
 }) {
+  const mobileAnimationProps = isMobile
+    ? {
+        initial: { y: "100%", opacity: 0 },
+        animate: { y: 0, opacity: 1 },
+        exit: { y: "100%", opacity: 0 },
+        transition: { type: "spring", stiffness: 300, damping: 30 },
+      }
+    : {};
   return (
-    <section className={`data-inputs ${isHidden ? "hidden" : ""}`}>
-      <BasicForm
-        basicData={basicData}
-        onChange={onChange}
-        profilePic={profilePic}
-        handlePicUpload={handlePicUpload}
-        deletePic={deletePic}
-      />
-      <Summary summaryData={summaryData} onChange={onChange} />
-      <Profiles
-        profilesData={profilesData}
-        openModal={openModal}
-        openUpdateModal={openUpdateModal}
-        setIndexItem={setIndexItem}
-      />
-      <SectionDataInputs
-        icon={<IoBriefcaseOutline />}
-        sectionId={"experience"}
-        sectionTitle={"Experience"}
-        openModal={openModal}
-        modalTypes={modalTypes.EXPERIENCE}
-      >
-        {experienceData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item.company}
-            subtitle={item.position}
-            handleClick={() => {
-              openUpdateModal(modalTypes.EXPERIENCE);
-              setIndexItem(index);
-            }}
+    <AnimatePresence>
+      {(isMobile ? isHidden : true) && (
+        <Motion.section
+          className={`data-inputs`}
+          {...(isMobile ? mobileAnimationProps : {})}
+        >
+          <BasicForm
+            basicData={basicData}
+            onChange={onChange}
+            profilePic={profilePic}
+            handlePicUpload={handlePicUpload}
+            deletePic={deletePic}
           />
-        ))}
-      </SectionDataInputs>
-      <SectionDataInputs
-        icon={<PiGraduationCap />}
-        sectionId={"education"}
-        sectionTitle={"Education"}
-        openModal={openModal}
-        modalTypes={modalTypes.EDUCATION}
-      >
-        {educationData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item.institution}
-            subtitle={item.study}
-            handleClick={() => {
-              openUpdateModal(modalTypes.EDUCATION);
-              setIndexItem(index);
-            }}
+          <Summary summaryData={summaryData} onChange={onChange} />
+          <Profiles
+            profilesData={profilesData}
+            openModal={openModal}
+            openUpdateModal={openUpdateModal}
+            setIndexItem={setIndexItem}
           />
-        ))}
-      </SectionDataInputs>
-      <SectionDataInputs
-        icon={<IoExtensionPuzzleOutline />}
-        sectionId={"projects"}
-        sectionTitle={"Projects"}
-        openModal={openModal}
-        modalTypes={modalTypes.PROJECT}
-      >
-        {projectsData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item.name}
-            subtitle={item.description}
-            handleClick={() => {
-              openUpdateModal(modalTypes.PROJECT);
-              setIndexItem(index);
-            }}
-          />
-        ))}
-      </SectionDataInputs>
-      <SectionDataInputs
-        icon={<GiSkills />}
-        sectionId={"skills"}
-        sectionTitle={"Skills"}
-        openModal={openModal}
-        modalTypes={modalTypes.SKILL}
-      >
-        {skillData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item}
-            handleClick={() => {
-              openUpdateModal(modalTypes.SKILL);
-              setIndexItem(index);
-            }}
-          />
-        ))}
-      </SectionDataInputs>
-      <SectionDataInputs
-        icon={<PiCertificateLight />}
-        sectionId={"certifications"}
-        sectionTitle={"Certifications"}
-        openModal={openModal}
-        modalTypes={modalTypes.CERTIFICATION}
-      >
-        {certificationData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item.name}
-            subtitle={item.issuer}
-            handleClick={() => {
-              openUpdateModal(modalTypes.CERTIFICATION);
-              setIndexItem(index);
-            }}
-          />
-        ))}
-      </SectionDataInputs>
-      <SectionDataInputs
-        icon={<IoLanguage />}
-        sectionId={"languages"}
-        sectionTitle={"Languages"}
-        openModal={openModal}
-        modalTypes={modalTypes.LANGUAGE}
-      >
-        {languageData?.map((item, index) => (
-          <ItemCard
-            key={index}
-            title={item}
-            handleClick={() => {
-              openUpdateModal(modalTypes.LANGUAGE);
-              setIndexItem(index);
-            }}
-          />
-        ))}
-      </SectionDataInputs>
-    </section>
+          <SectionDataInputs
+            icon={<IoBriefcaseOutline />}
+            sectionId={"experience"}
+            sectionTitle={"Experience"}
+            openModal={openModal}
+            modalTypes={modalTypes.EXPERIENCE}
+          >
+            {experienceData?.map((item, index) => (
+              <ItemCard
+                key={index}
+                title={item.company}
+                subtitle={item.position}
+                handleClick={() => {
+                  openUpdateModal(modalTypes.EXPERIENCE);
+                  setIndexItem(index);
+                }}
+              />
+            ))}
+          </SectionDataInputs>
+          <SectionDataInputs
+            icon={<PiGraduationCap />}
+            sectionId={"education"}
+            sectionTitle={"Education"}
+            openModal={openModal}
+            modalTypes={modalTypes.EDUCATION}
+          >
+            {educationData?.map((item, index) => (
+              <ItemCard
+                key={index}
+                title={item.institution}
+                subtitle={item.study}
+                handleClick={() => {
+                  openUpdateModal(modalTypes.EDUCATION);
+                  setIndexItem(index);
+                }}
+              />
+            ))}
+          </SectionDataInputs>
+          <SectionDataInputs
+            icon={<IoExtensionPuzzleOutline />}
+            sectionId={"projects"}
+            sectionTitle={"Projects"}
+            openModal={openModal}
+            modalTypes={modalTypes.PROJECT}
+          >
+            {projectsData?.map((item, index) => (
+              <ItemCard
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                handleClick={() => {
+                  openUpdateModal(modalTypes.PROJECT);
+                  setIndexItem(index);
+                }}
+              />
+            ))}
+          </SectionDataInputs>
+          <SectionDataInputs
+            icon={<GiSkills />}
+            sectionId={"skills"}
+            sectionTitle={"Skills"}
+            openModal={openModal}
+            modalTypes={modalTypes.SKILL}
+          >
+            {skillData?.map((item, index) => (
+              <ItemCard
+                key={index}
+                title={item}
+                handleClick={() => {
+                  openUpdateModal(modalTypes.SKILL);
+                  setIndexItem(index);
+                }}
+              />
+            ))}
+          </SectionDataInputs>
+          <SectionDataInputs
+            icon={<PiCertificateLight />}
+            sectionId={"certifications"}
+            sectionTitle={"Certifications"}
+            openModal={openModal}
+            modalTypes={modalTypes.CERTIFICATION}
+          >
+            {certificationData?.map((item, index) => (
+              <ItemCard
+                key={index}
+                title={item.name}
+                subtitle={item.issuer}
+                handleClick={() => {
+                  openUpdateModal(modalTypes.CERTIFICATION);
+                  setIndexItem(index);
+                }}
+              />
+            ))}
+          </SectionDataInputs>
+          <SectionDataInputs
+            icon={<IoLanguage />}
+            sectionId={"languages"}
+            sectionTitle={"Languages"}
+            openModal={openModal}
+            modalTypes={modalTypes.LANGUAGE}
+          >
+            {languageData?.map((item, index) => (
+              <ItemCard
+                key={index}
+                title={item}
+                handleClick={() => {
+                  openUpdateModal(modalTypes.LANGUAGE);
+                  setIndexItem(index);
+                }}
+              />
+            ))}
+          </SectionDataInputs>
+        </Motion.section>
+      )}
+    </AnimatePresence>
   );
 }
 
