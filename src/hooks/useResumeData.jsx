3e39d6199ab6b9350a9e12f350ae1c984ea996fb
bima-taruna <bimataruna21@gmail.com>
2025-07-15 11:24:15 +0700

    refactor: integrate useResumeStore for state management and remove localStorage dependencies

--- src/hooks/useResumeData.jsx
+++ src/hooks/useResumeData.jsx
@@ -1,24 +1,20 @@
 import { useState } from "react";
-import data from "../data/dummy-data";
+// import data from "../data/dummy-data";
 import { useEffect } from "react";
+import { useResumeStore } from "../stores/useResumeStore";
 
 const useResumeData = () => {
-  const [resumeData, setResumeData] = useState(() => {
-    const saved = localStorage.getItem("resumeData");
-    if (saved) return JSON.parse(saved);
-    return {
-      basic: data.basic,
-      summary: data.summary,
-      experience: data.experience,
-      profiles: data.profiles,
-      education: data.education,
-      projects: data.projects,
-      skills: data.skills,
-      certifications: data.certifications,
-      languages: data.languages,
-    };
-  });
-  const [profilePic, setProfilePic] = useState(null);
+  const {
+    resumeData,
+    changeBasicData,
+    addItem,
+    updateItem,
+    deleteItem,
+    profilePic,
+    setProfilePic,
+    deleteProfilePic,
+  } = useResumeStore();
+
   const [activeModal, setActiveModal] = useState(null);
   const [activeUpdateModal, setActiveUpdateModal] = useState(null);
   const [showPDF, setShowPDF] = useState(true);
@@ -29,10 +25,6 @@ const useResumeData = () => {
   const [indexItem, setIndexItem] = useState(null);
 
   useEffect(() => {
-    const savedPic = localStorage.getItem("profilePic");
-    const savedResumeData = localStorage.getItem("resumeData");
-    if (savedPic) setProfilePic(savedPic);
-    if (savedResumeData) setResumeData(JSON.parse(savedResumeData));
     const handleResize = () => {
       setIsMobile(window.innerWidth < 1024);
     };
@@ -40,32 +32,9 @@ const useResumeData = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    if (resumeData) {
-      localStorage.setItem("resumeData", JSON.stringify(resumeData));
-    }
-  }, [resumeData]);
   const handleChange = (e) => {
     const { name, value, id } = e.target;
-
-    setResumeData((prev) => {
-      switch (id) {
-        case "summary":
-          return {
-            ...prev,
-            summary: value,
-          };
-
-        default:
-          return {
-            ...prev,
-            basic: {
-              ...prev.basic,
-              [name]: value,
-            },
-          };
-      }
-    });
+    changeBasicData(id, name, value);
   };
 
   const handleSubmit = (e) => {
@@ -73,14 +42,10 @@ const useResumeData = () => {
     const id = e.target.id;
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData.entries());
-    setResumeData((prev) => ({
-      ...prev,
-      [id.split("-")[0]]: [
-        ...prev[id.split("-")[0]],
-        id === "skills-form" || id === "languages-form" ? data.name : data,
-      ],
-    }));
-
+    addItem(
+      id.split("-")[0],
+      id === "skills-form" || id === "languages-form" ? data.name : data
+    );
     setActiveModal(null);
   };
 
@@ -90,18 +55,17 @@ const useResumeData = () => {
 
     const reader = new FileReader();
     reader.onload = () => {
-      const base64 = reader.result;
-      localStorage.setItem("profilePic", base64);
-      setProfilePic(base64);
+      setProfilePic(reader.result);
     };
     reader.readAsDataURL(file);
   };
 
   const deletePic = () => {
     setShowPDF(false);
-    setProfilePic(null);
-    localStorage.removeItem("profilePic");
-    setTimeout(() => setShowPDF(true), 1);
+    setTimeout(() => {
+      deleteProfilePic();
+      setShowPDF(true);
+    }, 50);
   };
 
   const handleUpdateSubmit = (e) => {
@@ -109,20 +73,16 @@ const useResumeData = () => {
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData.entries());
     const id = e.target.id;
-    setResumeData((prev) => {
-      const updatedData = [...prev[id.split("-")[0]]];
-      updatedData[indexItem] =
-        id === "skills-form" || id === "languages-form" ? data.name : data;
-      return { ...prev, [id.split("-")[0]]: updatedData };
-    });
+    updateItem(
+      id.split("-")[0],
+      indexItem,
+      id === "skills-form" || id === "languages-form" ? data.name : data
+    );
     setActiveUpdateModal(null);
   };
 
   const handleDelete = (type, index) => {
-    setResumeData((prev) => ({
-      ...prev,
-      [type]: prev[type].filter((_, i) => i !== index),
-    }));
+    deleteItem(type, index);
     setActiveUpdateModal(null);
   };
 
