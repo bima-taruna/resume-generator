78397f197c531199617db6938578a2525ccdb48b
bima-taruna <bimataruna21@gmail.com>
2025-07-15 12:50:32 +0700

    refactor: update PdfDownload component to utilize useResumeStore for improved state management

--- src/components/PDF/PdfDownload.jsx
+++ src/components/PDF/PdfDownload.jsx
@@ -1,16 +1,20 @@
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import ResumePDF from "./ResumePDF";
+import { useResumeStore } from "../../stores/useResumeStore";
 
-const PdfDownload = ({ resumeData, profilePic }) => (
-  <div>
-    <PDFDownloadLink
-      style={{ color: "black", textDecoration: "none" }}
-      document={<ResumePDF {...resumeData} profilePic={profilePic} />}
-      fileName="resume.pdf"
-    >
-      {({ loading }) => (loading ? "Loading document..." : "Download PDF")}
-    </PDFDownloadLink>
-  </div>
-);
+const PdfDownload = () => {
+  const { resumeData, profilePic } = useResumeStore();
+  return (
+    <div>
+      <PDFDownloadLink
+        style={{ color: "black", textDecoration: "none" }}
+        document={<ResumePDF resumeData={resumeData} profilePic={profilePic} />}
+        fileName="resume.pdf"
+      >
+        {({ loading }) => (loading ? "Loading document..." : "Download PDF")}
+      </PDFDownloadLink>
+    </div>
+  );
+};
 
 export default PdfDownload;
